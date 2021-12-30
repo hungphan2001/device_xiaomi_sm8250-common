7224e1f14fefdd4a1994767243b946f52280d91b
ArixElo <patroxgamer@gmail.com>
2021-12-30 22:18:23 +0100

    sm8250-common: Build more libstagefright packages It fixes the: F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.media.omx@1.0-service": cannot locate symbol "_ZN7android25MEDIA_MIMETYPE_IMAGE_AVIFE" referenced by "/apex/com.android.vndk.v31/lib/libstagefright_omx_utils.so"...
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- kona.mk
+++ kona.mk
@@ -554,6 +554,8 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw \
+    libstagefrighthw_omx \
+    libstagefrighthw_foundation \
     libstagefright_omx.vendor
 
 # Perf
