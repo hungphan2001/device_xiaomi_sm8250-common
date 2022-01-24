030502d92d9c2857a6a891985aaa4321717cc2b5
Cristian Vázquez <cristianvaz81993@gmail.com>
2022-01-24 12:59:34 +0100

    Revert "sm8250-common: Switch to crDroid doze package"
    Bring back default doze package
    
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>
    Signed-off-by: Anujbarwal <anujbarwal2000@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
+import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -33,6 +34,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        DozeUtils.checkDozeService(context);
         TouchSamplingUtils.restoreSamplingValue(context);
     }
 }
