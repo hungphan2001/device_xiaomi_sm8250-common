2ca20ac6b13371b357f840f8f764669c81d0a141
Cristian Vázquez <cristianvaz81993@gmail.com>
2022-03-12 10:45:12 +0100

    sm8250-common: uprev android.hardware.audio to 7.0
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- kona.mk
+++ kona.mk
@@ -143,9 +143,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0 \
     android.hardware.audio.common@6.0 \
     android.hardware.audio.common@6.0-util \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@6.0 \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio@6.0-service \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0 \
