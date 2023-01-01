0d14c99568bfd737f6810eaa609273799c10a6ff
EmanuelCN <emanuelgithub@gmail.com>
2023-01-01 12:31:43 +0000

    sm8250-common: Nuke high polling rate
    *Enabled in kernel

--- kona.mk
+++ kona.mk
@@ -441,10 +441,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 endif
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
